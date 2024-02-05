41ecb5d01e7ac012fa27f252db0a71d2d5668126
Soumyadeep Ghosh <thekillermax1999@gmail.com>
2024-02-05 22:00:58 +0530

    updated routes

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const { getTodo, addTodo, removeTodo, updateTodo } = require("./crud");
 const { verifyToken } = require("./auth");
 
 router.get("/", function (req, res) {
-  res.json({ status: 200, message: "APi Connected" });
+  res.sendStatus(200).json({ message: "APi Connected" });
 });
 
 router.get("/getall", async function (req, res) {
@@ -19,7 +19,7 @@ router.get("/username/:input", async (req, res) => {
 
 router.post("/register", async function (req, res) {
   const result = await register(req.body);
-  if (result) res.json({ status: 200, message: "Successfully registered" });
+  if (result) res.sendStatus(200).json({ message: "Successfully registered" });
 });
 
 router.post("/login", async (req, res) => {
@@ -27,34 +27,38 @@ router.post("/login", async (req, res) => {
   res.json(result);
 });
 
-router.get("/json",middleware, async (req, res) => {
+router.get("/json", middleware, async (req, res) => {
   if (req.decoded) res.json(await getTodo(req.decoded));
   else res.sendStatus(403);
 });
 
-router.post("/json",middleware, async (req, res) => {
-  if(req.decoded) res.json(await addTodo(req.decoded, req.body));
+router.post("/json", middleware, async (req, res) => {
+  if (req.decoded) res.json(await addTodo(req.decoded, req.body));
   else res.sendStatus(403);
 });
 
-router.delete("/json",middleware, async (req, res) => {
-  if(req.decoded) res.json(await removeTodo(req.decoded, req.body));
-  else res.sendStatus(403)
+router.delete("/json", middleware, async (req, res) => {
+  if (req.decoded) res.json(await removeTodo(req.decoded, req.body));
+  else res.sendStatus(403);
 });
 
-router.patch("/json",middleware, async (req, res) => {
-  if(req.decoded) res.json(await updateTodo(req.decoded, req.body));
-  else res.sendStatus(403)
+router.patch("/json", middleware, async (req, res) => {
+  if (req.decoded) res.json(await updateTodo(req.decoded, req.body));
+  else res.sendStatus(403);
 });
 
-router.get("/test",middleware, async (req, res) => {
-
+router.get("/test", middleware, async (req, res) => {
   res.json(req.decoded);
+  // res.end()
 });
 
 function middleware(req, res, next) {
-  let data = req.headers.authorization.split(" ");
-  req.decoded = verifyToken(data[data.length - 1]);
+  if (req.headers.authorization) {
+    let data = req.headers.authorization.split(" ");
+    req.decoded = verifyToken(data[data.length - 1]);
+  }
+  else
+    req.decoded = false;
   next();
 }
 
